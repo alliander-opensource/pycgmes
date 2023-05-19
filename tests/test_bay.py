db24938327e6c44286369b03662994f47cf45f13
Guillaume Roger <guillaume.roger@alliander.com>
2023-05-19 16:42:01 +0200

    CI

---
+++ tests/test_bay.py
@@ -0,0 +1,55 @@
+import textwrap
+
+from src import Base, Bay, ConnectivityNodeContainer, EquipmentContainer, IdentifiedObject, PowerSystemResource
+
+
+class TestBay:
+    def test_load_bay(self):
+        b = Bay()
+
+    def test_bay_has_mrid(self):
+        b = Bay()
+        assert getattr(b, "mRID") is not None
+
+    def test_bay_has_expected_parents(self):
+        parents = Bay.__mro__[0:6]  # after that there's object.
+        expected = (  # mro returns a tuple
+            Bay,
+            EquipmentContainer,
+            ConnectivityNodeContainer,
+            PowerSystemResource,
+            IdentifiedObject,
+            Base,
+        )
+        assert expected == parents
+
+    def test_bay_has_expected_str(self):
+        expected = textwrap.dedent(
+            """
+        class=Bay
+        serializationProfile={}
+        description=
+        energyIdentCodeEic=
+        mRID=
+        name=
+        shortName=
+        VoltageLevel=None
+        """
+        )[
+            1:
+        ]  # The first character is a newline, which is not in str()
+        assert str(Bay()) == expected
+
+    def test_bay_has_expected_profiles(self):
+        profiles = Base().profiles
+        expected = {
+            "class": [
+                profiles.EQBD.value,
+                profiles.EQ.value,
+            ],
+            "VoltageLevel": [
+                profiles.EQBD.value,
+                profiles.EQ.value,
+            ],
+        }
+        assert expected == Bay().possible_profiles
---
+++ tests/test_init.py
@@ -0,0 +1,4 @@
+class TestInit:
+    def test_version_given(self):
+        # I just want to know that the version has been given.
+        from src import CGMES_VERSION
