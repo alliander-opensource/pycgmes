eff0f2f062fea50ee3790c0927b800af8c43e573
Guillaume Roger <guillaume.roger@alliander.com>
2023-06-29 14:48:38 +0200

    PR comments

--- SConstruct.py
+++ SConstruct.py
@@ -54,7 +54,9 @@ if "black" in COMMAND_LINE_TARGETS:
 if "ruff" in COMMAND_LINE_TARGETS or "lint" in COMMAND_LINE_TARGETS:
     # Replaces isort, autoflake and probably pylint eventually.
     for param in ["", "--extend-select RUF100"]:
-        # RUF100 means: remove #noqa when not relevant. A lot are adding during generation, it's nice to clean up.
+        # RUF100 means: remove #noqa when not relevant.
+        # A lot are adding during generation because due to comments, some lines can be too long. They do not all
+        # end up being too long, so let's clean up the not relevant one.
         cmd = f"ruff {_SUBJECT} {param}"
         if _CHECK_ONLY:
             cmd += " --no-fix"
