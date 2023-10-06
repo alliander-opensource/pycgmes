83889bbc8ca9212a9dcc0078e65df9abeec7485b
Gerrit van Tilburg <gerrit.van.tilburg@alliander.com>
2023-10-06 10:54:55 +0200

    add xsd namespace
    Signed-off-by: Gerrit van Tilburg <gerrit.van.tilburg@alliander.com>

--- pycgmes/resources/Base.py
+++ pycgmes/resources/Base.py
@@ -92,6 +92,7 @@ NAMESPACES = {
     "entsoe": "http://entsoe.eu/CIM/SchemaExtension/3/1#",
     "md": "http://iec.ch/TC57/61970-552/ModelDescription/1#",
     "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
 }
 
 
